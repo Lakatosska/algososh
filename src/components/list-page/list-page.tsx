7cd584c2e6e67a3225a0e99a13d92644e406374c
Elena Nikiforova <vizualnoe@gmail.com>
2022-09-03 22:21:15 +0300

    added queue and list pages layout

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,9 +1,52 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./list-page.module.css";
 
 export const ListPage: React.FC = () => {
+
+
   return (
     <SolutionLayout title="Связный список">
+      <section className={styles.form}>
+
+        <div className={styles.value}>
+        
+          <Input 
+            maxLength={4}
+            isLimitText
+            
+          />
+          <Button 
+            text='Добавить в head'
+          />
+          <Button 
+            text='Добавить в tail'
+          />
+          <Button 
+            text='Удалить из head'
+          />
+          <Button 
+            text='Удалить из tail'
+          />
+        </div>
+
+        <div className={styles.index}>
+          <Input 
+            maxLength={1}
+            
+          />
+          <Button 
+            text='Добавить по индексу'
+          />
+          <Button 
+            text='Удалить по индексу'
+          />
+        </div>
+      
+
+      </section>
 
     </SolutionLayout>
   );
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,9 +1,45 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./queue-page.module.css";
 
 export const QueuePage: React.FC = () => {
+
+  const onValueAdd = () => {
+    console.log('добавили значение')
+  }
+
+  const onValueDelete = () => {
+    console.log('удалили значение')
+  }
+
+  const onValuesClear = () => {
+    console.log('очистили стек')
+  }
+
   return (
     <SolutionLayout title="Очередь">
+      <section className={styles.form}>
+        <div className={styles.input}>
+          <Input 
+            maxLength={4}
+            isLimitText
+            
+          />
+          <Button onClick={onValueAdd}
+            text='Добавить'
+          />
+          <Button onClick={onValueDelete}
+            text='Удалить'
+          />
+        </div>
+
+        <Button onClick={onValuesClear}
+          text='Очистить'
+        />
+
+      </section>
 
     </SolutionLayout>
   );
